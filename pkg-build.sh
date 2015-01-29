7c746ac2a57093ccdfb4194c8b46c5a88a82c01a
hb <hb@aroma-project.org>
2015-01-28 16:20:38 -0800

    Tweaking apt settings

--- pkg-build.sh
+++ pkg-build.sh
@@ -110,10 +110,9 @@ BootstrapLinux() {
     # Change permissions for /usr/local/lib/R/site-library
     # This should really be via 'staff adduser travis staff'
     # but that may affect only the next shell
-    ls -l /usr/local/lib/
-    ls -l /usr/local/lib/R/
-    ls -l /usr/local/lib/R/site-library
-    sudo chmod 2777 /usr/local/lib/R /usr/local/lib/R/site-library
+    ls -l "${BINDIR}/R-${RVERSION}/"
+    ls -R "${BINDIR}/R-${RVERSION}/"
+#    sudo chmod 2777 /usr/local/lib/R /usr/local/lib/R/site-library
 
     # Process options
     BootstrapLinuxOptions
